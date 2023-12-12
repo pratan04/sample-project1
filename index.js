68026419c7082ad1fd36e3e6b24acc97c9887e81
pratan04 <35501178+pratan04@users.noreply.github.com>
2023-12-12 20:03:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser  from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { Console } from 'console';
+import postRoutes from './routes/posts.js';
+const app=express();
+
+app.use('/posts',postRoutes);
+
+app.use(bodyParser.json({limit: "30mb",extended:true}));
+
+app.use(bodyParser.urlencoded({limit: "30mb",extended:true}));
+app.use(cors());
+
+
+
+
+const CONNECTION_URL = 'mongodb+srv://shoonyaekai:Shoonya@12@cluster0.sviriwd.mongodb.net/?retryWrites=true&w=majority';
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser: true,unifiedTopology: true})
+
+.then(()=>app.listen(PORT,()=>Console.log('server running on port: $(PORT)')))
+.catch((error)=>console.log(error.message));
+
+mongoose.set('useFindAndModify',false);
